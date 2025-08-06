ea6cc672d6c7680064f4e66e3322c06a592b062a
Lakshya <lakshyajannu@gmail.com>
2025-08-06 18:41:58 -0400

    deepclean

--- server/services/battle-manager-instance.ts
+++
@@ -1,7 +0,0 @@
-import { BattleManager } from './battle-manager';
-
-/**
- * Singleton instance of BattleManager for centralized battle management
- * This ensures all components use the same instance for managing battles
- */
-export const battleManager = new BattleManager(); 
